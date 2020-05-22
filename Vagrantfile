cd156d8513d17c0469d28e8462ce6a1d0d968f81
Mattias Appelgren <mattias@ppelgren.se>
2020-05-23 00:17:20 +0200

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,38 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+#default_box = "ubuntu/focal64"
+default_box = "generic/ubuntu2004"
+default_memory = "2048"
+
+hosts = [
+  {
+    "name": "node-0",
+    "ip": "172.22.2.10",
+  },
+  {
+    "name":"node-1",
+    "ip": "172.22.2.11",
+  },
+]
+
+Vagrant.configure("2") do |config|
+
+hosts.each do |host|
+  name = host[:name]
+  config.vm.define name, primary: host.fetch(:primary, false) do |node|
+    node.vm.hostname = name
+    node.vm.box = host.fetch(:box, default_box)
+    #node.vm.synced_folder ".", "/vagrant", disabled: true
+    node.vm.network "private_network", ip: host[:ip]
+
+    node.vm.provider "virtualbox" do |vb|
+      vb.memory = host.fetch(:memory, default_memory)
+    end
+
+    node.vm.provision "shell", path: "setup.sh"
+
+  end
+end
+
+end
---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+snap install go --classic
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common \
+    gnupg2 \
+    golang-cfssl \
+    curl \
+    git \
+    jq
+
+install_docker() {
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" >/etc/apt/sources.list.d/docker.list
+    apt-get update -y
+    apt-cache policy docker-ce
+    apt-get install -y docker-ce
+    systemctl status docker
+    usermod -aG docker vagrant
+}
+install_docker
+
+install_k8s() {
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
+    apt-get update -y
+    apt-get install -y kubectl
+    apt-mark hold kubectl
+}
+install_k8s
+
+install_kind() {
+    local version="$1"
+    curl -sLo /usr/local/bin/kind "https://kind.sigs.k8s.io/dl/v${version}/kind-$(uname)-amd64"
+    chmod +x /usr/local/bin/kind
+    kind completion bash >/etc/bash_completion.d/kind
+}
+install_kind "0.8.1"
+
+clone_dj_kubelet_repos() {
+    mkdir -p /var/lib/dj-kubelet
+    cd /var/lib/dj-kubelet
+    curl -s https://api.github.com/orgs/dj-kubelet/repos |
+        jq -r '.[].clone_url' |
+        xargs -n1 git clone --recursive
+}
+clone_dj_kubelet_repos
